2b11afcebadd2ce47af52d9e793c722235f65d29
Angela Xu <axu@evermight.com>
2015-08-05 23:19:49 -0400

    removed files

--- public_html/model/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('asset/header.php');
-
-	function load(){
-		$arrMessage = call_api("discussion","get");
-		$results = array('arrMessage' => $arrMessage);
-		return $results;
-	}
-?>
